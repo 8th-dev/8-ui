fd866c5457dca6cb8adfa0a545632d3773d2770e
fRNRT8 <fran.am8@gmail.com>
2026-09-06 01:03:02 +0100

    feat: add Badge and Card components with respective exports

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@
 
 // Export all components
 export * from './components/ui/button'
+export * from './components/ui/badge'
+export * from './components/ui/card'
 export * from './components/ui/input'
 export * from './components/ui/label'
 export * from './components/ui/separator'
